31677cc84aaf963e11f8896c69961f110a4013f9
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2026-04-21 07:29:44 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -1,21 +1,19 @@
-#!/bin/sh
-# Corrected setup for macOS + Linux
-
+@echo off
+color 0a
 cd ..
+echo Installing dependencies...
+echo This might take a few moments depending on your internet speed.
 
-echo "Setting up haxelib folder..."
-mkdir -p ~/haxelib
-haxelib setup ~/haxelib
-
-echo "Installing core dependencies..."
+:: Core engine libs
 haxelib install lime 8.2.2
 haxelib install openfl 9.4.0
 haxelib install flixel 5.7.0
 haxelib install flixel-addons 3.4.0
+haxelib set flixel-addons 3.4.0
 haxelib install flixel-tools
 haxelib install flixel-ui
 
-echo "Installing scripting + utility libs..."
+:: Scripting + utilities
 haxelib install hscript 2.6.0
 haxelib install tjson
 haxelib install json2object
@@ -23,22 +21,23 @@ haxelib install json5hx
 haxelib install hxp
 haxelib install format
 
-echo "Installing native libs..."
+:: Native libs
 haxelib install hxcpp
 haxelib install hxcpp-debug-server
 haxelib install hxdiscord_rpc
 haxelib install hxvlc
 haxelib install hashlink
 
-echo "Installing git-based libs..."
+:: Git-based libs
 haxelib git flixel-animate https://github.com/MaybeMaru/flixel-animate.git
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate.git
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis.git
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git
 
-echo "Installing HaxeUI (matching versions)..."
+:: HaxeUI (matching versions)
 haxelib git haxeui-core https://github.com/haxeui/haxeui-core.git
 haxelib git haxeui-flixel https://github.com/haxeui/haxeui-flixel.git
 
-echo "Setup complete!"
+echo Finished!
+pause
