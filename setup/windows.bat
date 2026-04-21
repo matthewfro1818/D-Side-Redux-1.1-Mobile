4a3690ac454997f83f7c668f343c18fc9cf2c806
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2026-04-21 07:25:21 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -3,32 +3,40 @@ color 0a
 cd ..
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
+
+:: Core engine libs
+haxelib install lime 8.2.2
+haxelib install openfl 9.4.0
 haxelib install flixel 5.7.0
-haxelib install flixel-addons 3.3.0
-haxelib set flixel-addons 3.3.0
-haxelib install flixel-tools 1.5.1
+haxelib install flixel-addons 3.4.0
+haxelib install flixel-tools
 haxelib install flixel-ui
-haxelib install hscript-iris 1.1.3
-haxelib install tjson 1.4.0
-haxelib install hxdiscord_rpc 1.2.4
-haxelib install hxvlc 2.2.6
-haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
+
+:: Scripting + utilities
 haxelib install hscript 2.6.0
-haxelib install hxcpp
-haxelib install hxcpp-debug-server 1.2.4
-haxelib install json5hx 1.0.2
-haxelib install moonchart
+haxelib install tjson
 haxelib install json2object
-haxelib git flixel-animate https://github.com/MaybeMaru/flixel-animate.git
-haxelib git hscript-iris https://github.com/pisayesiwsi/hscript-iris dev
-haxelib git haxeui-flixel https://github.com/haxeui/haxeui-flixel 100f2c96beab619cfe72c567a058c41c71e3e998
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
-haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
-haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
-haxelib install format
+haxelib install json5hx
 haxelib install hxp
+haxelib install format
+
+:: Native libs
+haxelib install hxcpp
+haxelib install hxcpp-debug-server
+haxelib install hxdiscord_rpc
+haxelib install hxvlc
 haxelib install hashlink
+
+:: Git-based libs
+haxelib git flixel-animate https://github.com/MaybeMaru/flixel-animate.git
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate.git
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
+haxelib git funkin.vis https://github.com/FunkinCrew/funkVis.git
+haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git
+
+:: HaxeUI (matching versions)
+haxelib git haxeui-core https://github.com/haxeui/haxeui-core.git
+haxelib git haxeui-flixel https://github.com/haxeui/haxeui-flixel.git
+
 echo Finished!
 pause
